45a8afbf1fbe01f02ccb24b07bc046bbb458f454
TkhiienLok <tkhiien@gmail.com>
2020-06-19 17:36:26 +0300

    Moved toggling important and done properties to app lavel, added displaying counts in app header

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,44 +4,111 @@ import AppHeader from '../app-header';
 import TodoList from '../todo-list'
 import SearchPanel from '../search-panel'
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
 export default class App extends Component {
   constructor() {
     super();
+    this.maxId = 0;
+
+    this.createTodoItem = (label) => {
+      return { 
+        label: label, 
+        important: false,
+        done: false,
+        id: this.maxId++,
+      }
+    };
+
     this.state = {
       todoData: [
-        { label: 'Learn something', important: false, id: 1 }, 
-        { label: 'Create something', important: false, id: 2 }, 
-        { label: 'Drink capuchino', important: true, id: 3 }
+        this.createTodoItem('Learn something'),
+        this.createTodoItem('Create something'),
+        this.createTodoItem('Drink capuchino'),
       ]
-    }
+    };
+
     this.deleteItem = (id) => {
       this.setState(({todoData}) => {
         const idx = todoData.findIndex((el) => el.id === id);
-        const newtodoArray = [
+        const newTodoArray = [
           ...todoData.slice(0, idx), 
           ...todoData.slice(idx + 1)
         ];
         return {
-          todoData: newtodoArray
+          todoData: newTodoArray
         };
       });
-    }
+    };
+
+    this.addItem = (text) => {
+      this.setState(({ todoData }) => {
+        const newTodoArray = [
+          ...todoData, 
+          this.createTodoItem(text)
+        ];
+        return {
+          todoData: newTodoArray
+        };
+      });
+    };
+
+    this.toggleProperty = (arr, id, propName) => {
+      const idx = arr.findIndex((el) => el.id === id);
+      const oldItem = arr[idx];
+      const newItem = {
+        ...oldItem,
+        [propName]: !oldItem[propName]
+      };
+
+      const newArray = [
+          ...arr.slice(0, idx), 
+          newItem,
+          ...arr.slice(idx + 1)
+      ];
+      return newArray;
+    };
+
+    this.onToggleImportant = (id) => {
+      this.setState(({ todoData }) => {
+        const newTodoArray = this.toggleProperty(todoData, id, 'important');
+        return {
+          todoData: newTodoArray
+        };
+      });
+    };
+
+    this.onToggleDone = (id) => {
+      this.setState(({ todoData }) => {
+        const newTodoArray = this.toggleProperty(todoData, id, 'done');
+        return {
+          todoData: newTodoArray
+        };
+      });
+    };
   }
+
   render() {
+    const { todoData } = this.state;
+    const doneCount = todoData.filter((el) => el.done).length;
+    const todoCount = todoData.length - doneCount;
+
     return (
       <div className="todo-app">
         <span className="date-info">{ (new Date()).toString() }</span>
-        <AppHeader todo={1} done={3}/>
+        <AppHeader todo={todoCount} done={doneCount}/>
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter/>
         </div>
+        <ItemAddForm onItemAdded={this.addItem}/>
         <TodoList 
-          todos={this.state.todoData}
-          onDeleted={this.deleteItem}/>
+          todos={todoData}
+          onDeleted={this.deleteItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleDone={this.onToggleDone}/>
       </div>
     );
   }
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,7 +3,7 @@ import React from 'react';
 import TodoListItem from '../todo-list-item';
 import './todo-list.css';
 
-const TodoList = ({ todos, onDeleted }) => {
+const TodoList = ({ todos, onDeleted, onToggleImportant, onToggleDone }) => {
 	const elements = todos.map((item) => {
 		const { id, ...itemProps } = item;
 		return (
@@ -11,6 +11,8 @@ const TodoList = ({ todos, onDeleted }) => {
 				<TodoListItem 
 					{ ...itemProps }
 					onDeleted={() => onDeleted(id)}
+					onToggleImportant={() => onToggleImportant(id)}
+					onToggleDone={() => onToggleDone(id)}
 				/>
 			</li>
 		);
@@ -22,4 +24,4 @@ const TodoList = ({ todos, onDeleted }) => {
 	);
 };
 
-  export default TodoList;
+export default TodoList;
